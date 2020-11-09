880a2b0798166da0e10b23948eab886fbd7ad717
Tom Cannaerts <tom.cannaerts@intracto.com>
2020-11-09 17:06:50 +0100

    Less apache, use hosts

--- run.sh
+++ run.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-if [ "$2" == "" ]
-then
-	CONNECT=$1
-else
-	CONNECT=$2
-fi
-
 docker run --rm -ti \
-	-e PROXY_HOSTNAME=$1 -e PROXY_IP=$CONNECT \
+	-e PROXY_HOSTNAME=$1 \
 	-v $PWD/httpd.conf:/usr/local/apache2/conf/httpd.conf \
 	-v $PWD/cert.key:/etc/ssl/private/cert.key \
 	-v $PWD/cert.pem:/etc/ssl/certs/cert.pem \
 	-p 8080:80 \
 	-p 8443:443 \
+	--add-host $1:$2 \
 	httpd:alpine
